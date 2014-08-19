b5627ad161a8547accb5952664a8280711c2bd69
KurtRobo <kr185@uowmail.edu.au>
2014-08-19 19:42:27 +1000

    whatever

--- src/com/example/YoCatchAndroid/MyActivity.java
+++ src/com/example/YoCatchAndroid/MyActivity.java
@@ -1,50 +1,92 @@
 package com.example.YoCatchAndroid;
 
-import java.util.*;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import java.util.*;
+import android.graphics.Color;
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private String displayText;
+    private TextView myTextView;
+    private EditText yoField;
+    private EditText nameField;
+    private Button showMessageButton;
+    private LinearLayout myLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        final TextView myTextView = (TextView)findViewById(R.id.textView);
-        final EditText yoField = (EditText)findViewById(R.id.yoField);
-        final EditText nameField = (EditText)findViewById(R.id.nameField);
-        final Button showMessageButton = (Button)findViewById(R.id.showMessageButton);
+        displayText = "";
+        myTextView = (TextView) findViewById(R.id.textView);
+        yoField = (EditText) findViewById(R.id.yoField);
+        nameField = (EditText) findViewById(R.id.nameField);
+        showMessageButton = (Button) findViewById(R.id.showMessageButton);
+        myLayout = (LinearLayout) findViewById(R.id.myLayout);
 
         showMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myTextView.setText(yoField.getText() + "\n" + nameField.getText());
+                changeLabel();
             }
         });
 
-        yoField.setImeActionLabel("Custom text", KeyEvent.KEYCODE_ENTER);
-
-/*
-        yoField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-        }
-
+        yoField.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public boolean onEditorAction(TextView arg0, int keycode, KeyEvent event) {
-                if(arg1 == KeyEvent.FLAG_EDITOR_ACTION){
-                    btnSave.requestFocus();
-                    return true;
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            changeLabel();
+                            return true;
+                        default:
+                            break;
+                    }
                 }
                 return false;
+            }
+        });
 
-            });
-*/
+        nameField.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_DPAD_CENTER:
+                    case KeyEvent.KEYCODE_ENTER:
+                        changeLabel();
+                        return true;
+                    default:
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    public void changeLabel() {
+        if(!displayText.equals(yoField.getText() + "\n" + nameField.getText())) {
+            //Setting string to current label text
+            displayText = yoField.getText() + "\n" + nameField.getText();
+
+            System.out.println("hey");
+            //New color generated
+            Random rnd = new Random();
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            myLayout.setBackgroundColor(color);
+            myTextView.setText(displayText);
+        }
     }
 }
